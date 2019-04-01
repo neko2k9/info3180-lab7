88d6362522a56175ae83f583f73438fa9d324240
Neko Davis <neko.davis@mymona.uwi.edu>
2019-04-01 03:04:01 +0000

    Upload functionality for Vue

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Email
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[InputRequired(message='A description is required.')])
+    uploadImage = FileField('Upload Photo', validators=[FileRequired('Please select a photo to upload.'), FileAllowed(['jpg', 'jpeg', 'png'], 'Please upload image files only.')])
--- app/views.py
+++ app/views.py
@@ -5,8 +5,11 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request
+import os
+from app import app, filefolder
+from flask import render_template, request, jsonify
+from app.forms import UploadForm
+from werkzeug.utils import secure_filename
 
 ###
 # Routing for your application.
@@ -16,9 +19,11 @@ from flask import render_template, request
 # Please create all new routes and view functions above this route.
 # This route is now our catch all route for our VueJS single page
 # application.
+
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
+@app.route('/')
+def index():
+    form = UploadForm()
     """
     Because we use HTML5 history mode in vue-router we need to configure our
     web server to redirect all routes to index.html. Hence the additional route
@@ -26,8 +31,22 @@ def index(path):
 
     Also we will render the initial webpage and then let VueJS take control.
     """
-    return render_template('index.html')
+    return render_template('index.html', form=form)
 
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    form=UploadForm()
+    if request.method == "POST" and form.validate_on_submit():
+        description = request.form['description']
+        file = request.files['uploadImage']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(filefolder, filename))
+        result = [{'message': 'File Upload Successful', 'filename': filename, 'description': description}]
+        return jsonify(result=result)
+    else:
+        error_collection = form_errors(form)
+        error = [{'errors': error_collection}]
+        return  jsonify(errors=error)
 
 # Here we define a function to collect form errors from Flask-WTF
 # which we can later use
